43438748ca62beacb1301f97772e01a816150d65
RafaelCruz1 <rafaeloribata@gmail.com>
2023-04-06 14:27:41 -0300

    Adicionando classe products

---
+++ src/Entities/List.java
@@ -0,0 +1,10 @@
+package Entities;
+
+import java.util.ArrayList;
+
+public class List {
+   
+    ArrayList<Products> list = new ArrayList<>();
+
+
+}
---
+++ src/Entities/Products.java
@@ -0,0 +1,62 @@
+package Entities;
+
+import java.util.Objects;
+
+public class Products {
+
+    int id;
+    String name;
+    double price;
+    int quantity;
+
+    public Products() {
+    }
+
+    public Products(int id, String name, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Products)) return false;
+        Products products = (Products) o;
+        return id == products.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
